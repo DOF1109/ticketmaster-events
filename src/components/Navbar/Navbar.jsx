303f233a61c5e1dea842ef8666c30f1e0881122c
DOF1109 <friasdaniel.1109@gmail.com>
2023-12-12 16:09:40 -0300

    Pirmer componente creado

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return(
+        <>
+            <p>Eventos</p>
+            <input type="text" placeholder="Buscar evento" />
+        </>
+    );
+};
+
+export default Navbar;
